e3624228fd94d435ee366c3864868c63db6129e1
Kanghee Han <hcsy1219@gmail.com>
2023-08-28 23:58:47 +0900

    create project

---
+++ src/main/kotlin/com/example/GraphQL_Practice/GraphQlPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.GraphQL_Practice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GraphQlPracticeApplication
+
+fun main(args: Array<String>) {
+	runApplication<GraphQlPracticeApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/GraphQL_Practice/GraphQlPracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.GraphQL_Practice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GraphQlPracticeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
